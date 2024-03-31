98cc328d7c77ef8994e740247542c0f5ad3b68b5
Kevin de Vries <kevin@aubergine-it.nl>
2024-03-31 19:06:46 +0200

    Move nvim-config submodule content into this repository

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '^4.3',
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons' },
+    },
+    config = function ()
+      require("bufferline").setup({})
+    end,
+  }
+}
---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.6',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function ()
+      local telescope = require('telescope')
+      telescope.setup({
+        pickers = {
+          find_files = {
+            find_command = {
+              "rg",
+              "--no-ignore",
+              "--hidden",
+              "--files",
+              "-g",
+              "!**/node_modules/*",
+              "-g",
+              "!**/.git/*",
+            },
+          },
+        }
+      })
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+      -- Set `fd` as an alias for `find_files` again
+      builtin.fd = builtin.find_files
+    end
+  }
+}
