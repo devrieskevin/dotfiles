7c224d664148e070c9405180ee6473d75d37b8d9
Kevin de Vries <kevin@aubergine-it.nl>
2024-04-01 01:00:10 +0200

    Specify luasnip as cmp engine in snippet expand function

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -5,6 +5,12 @@ return {
             local cmp = require('cmp')
 
             cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                    end,
+                },
                 sources = cmp.config.sources(
                     {
                         { name = 'copilot' },
