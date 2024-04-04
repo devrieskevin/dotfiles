942c10e1fec628be7b627bb301fd7e04b42e1628
Kevin de Vries <kevin@aubergine-it.nl>
2024-04-05 00:57:30 +0200

    Add DAP adapter and configuration config for PHP
    - Also provide a commented out executable based adapter for testing

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,5 +1,31 @@
 return {
-  { "mfussenegger/nvim-dap" },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+
+      -- dap.adapters.php = {
+      --   type = "executable",
+      --   command = vim.fn.stdpath("data") .. "/mason/bin/php-debug-adapter",
+      --   args = {},
+      -- }
+
+      dap.adapters.php = {
+        type = "executable",
+        command = "node",
+        args = { vim.fn.stdpath("data") .. "/mason/packages/php-debug-adapter/extension/out/phpDebug.js" },
+      }
+
+      dap.configurations.php = {
+        {
+          type = "php",
+          request = "launch",
+          name = "Listen for Xdebug",
+          port = 9003,
+        },
+      }
+    end,
+  },
   {
     "rcarriga/nvim-dap-ui",
     dependencies = {
