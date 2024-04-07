741f5013cc47b124efd1cc31115fb2cbc9da13be
Kevin de Vries <kevin@aubergine-it.nl>
2024-04-08 01:56:52 +0200

    Add neotree toggle keymap and show dotfiles + gitignored by default

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,5 +7,19 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     },
+    config = function()
+      local neotree = require("neo-tree")
+
+      neotree.setup({
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>nt", "<Cmd>Neotree toggle<CR>")
+    end,
   },
 }
