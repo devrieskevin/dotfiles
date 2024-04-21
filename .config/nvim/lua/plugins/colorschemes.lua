6f93b5b065e50cbf2f1c6d55a076b21ff0ef52d6
Kevin de Vries <kevin@aubergine-it.nl>
2024-04-21 20:34:52 +0200

    Use night style for tokyonight

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -2,6 +2,12 @@ return {
   {
     "folke/tokyonight.nvim",
     config = function()
+      local tokyonight = require("tokyonight")
+
+      tokyonight.setup({
+        style = "night",
+      })
+
       vim.cmd.colorscheme("tokyonight")
     end,
   },
