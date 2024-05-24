7cf4d4b118b825b530c8cad84e68a884a2ec9ae4
Kevin de Vries <kevin@aubergine-it.nl>
2024-05-24 22:43:57 +0200

    Also make block cursor blink

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,4 +8,4 @@ vim.opt.number = true
 vim.opt.listchars = { lead = "·" }
 vim.opt.list = true
 
-vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25-blinkwait175-blinkoff150-blinkon175-Cursor/lCursor,r-cr-o:hor20"
+vim.o.guicursor = "n-v-c-sm:block-blinkwait175-blinkoff150-blinkon175,i-ci-ve:ver25-blinkwait175-blinkoff150-blinkon175-Cursor/lCursor,r-cr-o:hor20"
