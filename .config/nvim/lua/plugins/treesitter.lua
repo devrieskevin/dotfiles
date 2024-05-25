e93f15ff42a7398e0e609bd19f8efc5bfb39c46f
Kevin de Vries <kevin@aubergine-it.nl>
2024-05-25 22:58:47 +0200

    Add move and swap keymaps and remove explanation comments

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -39,24 +39,46 @@ return {
             enable = true,
             lookahead = true,
             keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
               ["af"] = "@function.outer",
               ["if"] = "@function.inner",
               ["ac"] = "@class.outer",
-              -- you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
               ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
             },
-            -- You can choose the select mode (default is charwise 'v')
             selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "<c-v>", -- blockwise
+              ["@parameter.outer"] = "v",
+              ["@function.outer"] = "V",
+              ["@class.outer"] = "<c-v>",
             },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`. Can also be a function (see above).
             include_surrounding_whitespace = true,
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+              ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[s"] = { query = "@scope", query_group = "locals", desc = "Previous scope" },
+              ["[z"] = { query = "@fold", query_group = "folds", desc = "Previous fold" },
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+            },
           },
         },
       })
