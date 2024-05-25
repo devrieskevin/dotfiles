bb8e373f808acff8e066ada19b3f058c1c579122
Kevin de Vries <kevin@aubergine-it.nl>
2024-05-25 22:29:31 +0200

    Install treesitter textobjects plugin + configure select keymaps
    - Using the default config settings for now

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -27,4 +27,39 @@ return {
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              -- you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            selection_modes = {
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`. Can also be a function (see above).
+            include_surrounding_whitespace = true,
+          },
+        },
+      })
+    end,
+  },
 }
