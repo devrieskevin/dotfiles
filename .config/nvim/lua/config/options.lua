6dc57d79ec4db3936d54501e4223d90fb295ff90
Kevin de Vries <kevin@aubergine-it.nl>
2024-08-15 14:58:55 +0200

    Enable relativenumber + cursorline opts
    - For better ergonomics and profit

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,6 +5,8 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
 vim.opt.listchars = { lead = "·" }
 vim.opt.list = true
 
