19efea6e12232a2cba0d8af40c66bffd0a2c5413
Kevin de Vries <kevin@aubergine-it.nl>
2025-03-16 00:45:41 +0100

    Remove deprecated `folke/neodev.nvim` dependency

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -81,9 +81,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     version = "^1.7",
-    dependencies = {
-      { "folke/neodev.nvim" },
-    },
     config = function()
       vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
       vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
