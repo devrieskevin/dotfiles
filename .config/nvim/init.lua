c52f6df03ed4731a5a9a9bc32b1c628677aeda63
Kevin de Vries <kevin@aubergine-it.nl>
2026-07-03 23:54:28 +0200

    Remove lazy.nvim package manager from init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,5 @@
 require("config.options")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  performance = {
-    reset_packpath = false,
-  },
-})
-
 -- Enable builtin plugins
 vim.cmd("packadd nvim.difftool")
 vim.cmd("packadd nvim.undotree")
