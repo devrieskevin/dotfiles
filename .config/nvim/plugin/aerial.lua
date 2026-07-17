b93bd531f6d8a97d2fb7fd327dbd80e670af6803
Kevin de Vries <kevin@aubergine-it.nl>
2026-07-18 00:28:51 +0200

    Remove unnecessary telescope requirement from `vim.pack.add`

--- .config/nvim/plugin/aerial.lua
+++ .config/nvim/plugin/aerial.lua
@@ -1,5 +1,4 @@
 vim.pack.add({
-  "https://github.com/nvim-treesitter/nvim-treesitter",
   "https://github.com/nvim-tree/nvim-web-devicons",
   {
     src = "https://github.com/stevearc/aerial.nvim",
