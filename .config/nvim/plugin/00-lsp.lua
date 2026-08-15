3ff23bacc03895795fd06ed8aec7796d0d7d07fc
Kevin de Vries <kevin@aubergine-it.nl>
2026-08-16 00:00:56 +0200

    Use the right Mason identifier for PHPantom

--- .config/nvim/plugin/00-lsp.lua
+++ .config/nvim/plugin/00-lsp.lua
@@ -36,7 +36,7 @@ require("mason-lspconfig").setup({
   ensure_installed = {
     "lua_ls",
     "pyright",
-    "phpantom",
+    "phpantom_lsp",
     "ts_ls",
     "twiggy_language_server",
     "docker_compose_language_service",
